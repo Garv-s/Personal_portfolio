9202fe029d6c985d76f853b0e38516e14f50211e
Garv Shah <131426081+Garv-s@users.noreply.github.com>
2026-02-10 16:11:11 +0530

    Add StatsBar component

---
+++ src/components/StatsBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StatsBar = () => {
+    return (
+        <div className="stats-bar">
+            {/* Stats content goes here */}
+        </div>
+    );
+};
+
+export default StatsBar;
